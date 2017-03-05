273877ea9f0ac4264a8a8b7b379f285404d49151
Nil Geisweiller <ngeiswei@gmail.com>
2017-03-05 11:03:21 +0200

    Add Beats::relative_ticks
    like Beats::ticks but can be negative as well

--- libs/temporal/temporal/beats.h
+++ libs/temporal/temporal/beats.h
@@ -89,6 +89,11 @@ public:
 		return Beats (0, ticks);
 	}
 
+	/** Create from relative ticks at the standard PPQN. */
+	static Beats relative_ticks(int32_t relative_ticks) {
+		return Beats(relative_ticks / PPQN);
+	}
+
 	/** Create from ticks at a given rate.
 	 *
 	 * Note this can also be used to create from frames by setting ppqn to the
