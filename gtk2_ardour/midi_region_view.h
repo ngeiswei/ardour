df0413fdc63428691f73916a729d71b69eba4cdf
Nil Geisweiller <ngeiswei@gmail.com>
2017-05-26 23:11:32 +0300

    Fix typo in comments

--- gtk2_ardour/midi_region_view.h
+++ gtk2_ardour/midi_region_view.h
@@ -504,7 +504,7 @@ public:
 
 	std::vector<NoteResizeData *> _resize_data;
 
-	/** connection used to connect to model's ContentChanged signal */
+	/** connection used to connect to model's ContentsChanged signal */
 	PBD::ScopedConnection content_connection;
 
 	NoteBase* find_canvas_note (std::shared_ptr<NoteType>);
