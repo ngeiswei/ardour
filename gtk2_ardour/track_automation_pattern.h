3a8e43d03dc73f0eb1305e26389324a78efcbb39
Nil Geisweiller <ngeiswei@gmail.com>
2017-09-12 00:14:06 +0300

    Can edit newly added processors

--- gtk2_ardour/track_automation_pattern.h
+++ gtk2_ardour/track_automation_pattern.h
@@ -28,7 +28,7 @@ class TrackAutomationPattern : public AutomationPattern {
 public:
 	TrackAutomationPattern(ARDOUR::Session* session,
 	                       boost::shared_ptr<ARDOUR::Region> region,
-	                       const AutomationControlSet& automation_controls);
+	                       const AutomationControlSet& automation_controls=AutomationControlSet());
 
 	// Assign a control event to a row
 	virtual uint32_t event2row(const Evoral::Parameter& param, const Evoral::ControlEvent* event);
