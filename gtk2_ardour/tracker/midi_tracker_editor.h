acdb64e196344da44df251d91f0f5a1d3860c20d
Nil Geisweiller <ngeiswei@gmail.com>
2017-12-10 18:44:16 +0200

    Keep preparing multi track tracker

--- gtk2_ardour/tracker/midi_tracker_editor.h
+++ gtk2_ardour/tracker/midi_tracker_editor.h
@@ -98,7 +98,7 @@ class MidiTrackerEditor : public ArdourWindow
   public:
 	typedef Evoral::Note<Temporal::Beats> NoteType;
 
-	MidiTrackerEditor(ARDOUR::Session*, MidiTimeAxisView*, boost::shared_ptr<ARDOUR::Route>, MidiRegionView*);
+	MidiTrackerEditor(ARDOUR::Session*, RegionSelection& rs);
 	~MidiTrackerEditor();
 
   private:
