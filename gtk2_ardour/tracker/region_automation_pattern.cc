75a3014235f7ce985d24f629f6c2c8d95ceffffb
Nil Geisweiller <ngeiswei@gmail.com>
2017-12-16 18:00:15 +0200

    More file renaming

--- gtk2_ardour/tracker/region_automation_pattern.cc
+++ gtk2_ardour/tracker/region_automation_pattern.cc
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2016 Nil Geisweiller
+    Copyright (C) 2016-2017 Nil Geisweiller
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
