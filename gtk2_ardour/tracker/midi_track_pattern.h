f33e0045cb77342f61b45e471b91c1636238a0d4
Nil Geisweiller <ngeiswei@gmail.com>
2017-12-16 19:42:34 +0200

    Start implementing MidiTrackPattern

---
+++ gtk2_ardour/tracker/midi_track_pattern.h
@@ -0,0 +1,41 @@
+/*
+    Copyright (C) 2017 Nil Geisweiller
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __ardour_gtk2_tracker_track_pattern_h_
+#define __ardour_gtk2_tracker_track_pattern_h_
+
+#include "note_pattern.h"
+#include "region_automation_pattern.h"
+#include "track_automation_pattern.h"
+
+/**
+ * Represent all patterns, midi notes, midi automations and track automations
+ * of a given track, with possibly multiple regions, possibly overlapping, as
+ * long as they all come from the same track.
+ */
+class MidiTrackPattern {            // TODO: maybe inherit from BasePattern
+public:
+	MidiTrackPattern (ARDOUR::Session* session,
+	                  boost::shared_ptr<ARDOUR::MidiRegion> region);
+
+	NotePattern np;
+	RegionAutomationPattern rap;
+	TrackAutomationPattern tap;
+};
+
+#endif /* __ardour_gtk2_tracker_track_pattern_h_ */
