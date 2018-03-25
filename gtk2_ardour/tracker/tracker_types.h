622486b858f2ef20a04dbad8b14fe952efb725f4
Nil Geisweiller <ngeiswei@gmail.com>
2018-03-25 19:34:19 +0300

    Support multi track toolbar

---
+++ gtk2_ardour/tracker/tracker_types.h
@@ -0,0 +1,25 @@
+/*
+    Copyright (C) 2018 Nil Geisweiller
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __ardour_tracker_tracker_types_h_
+#define __ardour_tracker_tracker_types_h_
+
+// Map Parameter to AutomationControl
+typedef std::map<Evoral::Parameter, boost::shared_ptr<ARDOUR::AutomationControl> > Parameter2AutomationControl;
+
+#endif /* __ardour_tracker_tracker_types_h_ */
