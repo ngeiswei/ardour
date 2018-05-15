395a13e7c59239b18a96c0088234c6078e1606ba
Nil Geisweiller <ngeiswei@gmail.com>
2018-05-15 20:26:15 +0300

    Add current cursor move

---
+++ gtk2_ardour/tracker/tracker_column.cc
@@ -0,0 +1,50 @@
+/*
+    Copyright (C) 2018 Nil Geisweiller
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "tracker_column.h"
+
+#include "pbd/i18n.h"
+
+using namespace std;
+using namespace Gtk;
+
+TrackerColumn::TrackerColumn(const Glib::ustring& title,
+                             const TreeModelColumn<std::string>& column,
+                             int mti, int cgi,
+                             midi_note_type mnt, automation_type at)
+	: TreeViewColumn(title, column),
+	  midi_track_idx(mti), col_group_idx(cgi),
+	  note_type(mnt), auto_type(at) {}
+
+NoteColumn::NoteColumn(const TreeModelColumn<std::string>& column, int mti, int cgi)
+	: TrackerColumn(_("Note"), column, mti, cgi, NOTE, AUTOMATION_SEPARATOR) {}
+
+ChannelColumn::ChannelColumn(const TreeModelColumn<std::string>& column, int mti, int cgi)
+	: TrackerColumn(S_("Channel|Ch"), column, mti, cgi, CHANNEL, AUTOMATION_SEPARATOR) {}
+
+VelocityColumn::VelocityColumn(const TreeModelColumn<std::string>& column, int mti, int cgi)
+	: TrackerColumn(S_("Velocity|Vel"), column, mti, cgi, VELOCITY, AUTOMATION_SEPARATOR) {}
+
+DelayColumn::DelayColumn(const TreeModelColumn<std::string>& column, int mti, int cgi)
+	: TrackerColumn(_("Delay"), column, mti, cgi, DELAY, AUTOMATION_SEPARATOR) {}
+
+AutomationColumn::AutomationColumn(const TreeModelColumn<std::string>& column, int mti, int cgi)
+	: TrackerColumn("", column, mti, cgi, SEPARATOR, AUTOMATION) {}
+
+AutomationDelayColumn::AutomationDelayColumn(const TreeModelColumn<std::string>& column, int mti, int cgi)
+	: TrackerColumn(_("Delay"), column, mti, cgi, SEPARATOR, AUTOMATION_DELAY) {}
