2ed73b47eff95c5074144d6a8a0c0c297eadf0f2
Nil Geisweiller <ngeiswei@gmail.com>
2018-05-15 20:26:15 +0300

    Add current cursor move

---
+++ gtk2_ardour/tracker/tracker_column.h
@@ -0,0 +1,94 @@
+/*
+    Copyright (C) 2018 Nil Geisweiller
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+#ifndef __ardour_tracker_tracker_column_h_
+#define __ardour_tracker_tracker_column_h_
+
+#include <gtkmm/treeviewcolumn.h>
+#include <gtkmm/treemodelcolumn.h>
+
+class TrackerColumn : public Gtk::TreeViewColumn
+{
+public:
+	// Type of data the midi note column is holding. It also serve to calculate
+	// the column index as well.
+	enum midi_note_type {
+		NOTE,
+		CHANNEL,
+		VELOCITY,
+		DELAY,
+		SEPARATOR
+	}; 
+
+	// Type of data the automation column is hold. It also serve to calculate
+	// the column index as well.
+	enum automation_type {
+		AUTOMATION,
+		AUTOMATION_DELAY,
+		AUTOMATION_SEPARATOR
+	};
+
+	TrackerColumn(const Glib::ustring& title,
+	              const Gtk::TreeModelColumn<std::string>& column,
+	              int mti, int cgi,
+	              midi_note_type mnt, automation_type at);
+
+	int midi_track_idx; // midi track index
+	int col_group_idx; // either note or automation column group index
+	const enum midi_note_type note_type; // NOTE_SEPARATOR means inactive
+	const enum automation_type auto_type; // AUTOMATION_SEPARATOR means inactive
+};
+
+class NoteColumn : public TrackerColumn
+{
+public:
+	NoteColumn(const Gtk::TreeModelColumn<std::string>& column, int mti, int cgi);
+};
+
+class ChannelColumn : public TrackerColumn
+{
+public:
+	ChannelColumn(const Gtk::TreeModelColumn<std::string>& column, int mti, int cgi);
+};
+
+class VelocityColumn : public TrackerColumn
+{
+public:
+	VelocityColumn(const Gtk::TreeModelColumn<std::string>& column, int mti, int cgi);
+};
+
+class DelayColumn : public TrackerColumn
+{
+public:
+	DelayColumn(const Gtk::TreeModelColumn<std::string>& column, int mti, int cgi);
+};
+
+class AutomationColumn : public TrackerColumn
+{
+public:
+	AutomationColumn(const Gtk::TreeModelColumn<std::string>& column, int mti, int cgi);
+};
+
+class AutomationDelayColumn : public TrackerColumn
+{
+public:
+	AutomationDelayColumn(const Gtk::TreeModelColumn<std::string>& column, int mti, int cgi);
+};
+
+#endif /* __ardour_tracker_tracker_column_h_ */
