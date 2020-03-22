07de855138ce21479d069acd3887ddb919043eb1
Nil Geisweiller <ngeiswei@gmail.com>
2020-03-22 22:47:16 +0200

    Complete MIDI input support
    just need to support delay update when overwrite existing is enabled.

--- gtk2_ardour/selection.cc
+++ gtk2_ardour/selection.cc
@@ -1475,8 +1475,6 @@ Selection::remove (const TrackViewList& t)
 void
 Selection::set (TimeAxisView* track)
 {
-    // VVT: select track when changing track col
-    std::cout << "Selection::set (track=" << track << ")" << std::endl;
 	TrackViewList tvl;
 	tvl.push_back (track);
 	set (tvl);
